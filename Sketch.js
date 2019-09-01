4fad7c80c075504fab81edb33f91f1074f2d28a7
Odil Bright <MacbookPro@iMac-de-Prueba.local>
2019-09-01 21:48:11 +0200

    Revert "RootArpegiada"
    This reverts commit b21bb55a721b605d929779015629e2dc66746048.

--- Sketch.js
+++ Sketch.js
@@ -27,17 +27,14 @@ import {
     drawParticlesInScale,
     getModeScaleLength,
     getModeChordLength,
-    getRootLength,
     animateScaleParticle,
     animateChordParticle,
-    animateRootParticle,
 } from './Modes.js';
 
 
 var state = 'caos';
 var scaleIndex = 0;
 var chordIndex = 0;
-var rootIndex = 0;
 
 export const setup = () => {
     initCanvas();
@@ -79,26 +76,23 @@ const drawModeAnimationFrame = (mode,currentModes) => {
     stopParticlesInScale(mode);
     drawParticlesInScale(mode);
 
+    if (frameShouldAnimateRootParticle(frameCount)) {
+        setDefaultParticleColor();
+        animateRoot(frameCount);
+    }
+
     if (frameShouldAnimateScaleParticle(frameCount)) {
         animateScaleParticle(mode, scaleIndex, frameCount);
         scaleIndex++;
     }
     
-    if (frameShouldAnimateChordParticle(frameCount, mode)) {
-        if (chordIndex === getModeChordLength(mode)) {
+    if (frameShouldAnimateChordParticle(frameCount)) {
+        if (chordIndex === 10) {
             chordIndex = 0;
         }
         animateChordParticle(mode, chordIndex, frameCount);
         chordIndex++;
         
-    }
-    
-    if (frameShouldAnimateRootParticle(frameCount)) {
-        if (rootIndex === 5) {
-            rootIndex = 0;
-        }
-        animateRootParticle(rootIndex, frameCount);
-        rootIndex++;
     }
 
     if (frameShouldFinishModeAnimation(scaleIndex, mode)) {
@@ -114,7 +108,7 @@ const frameShouldAnimateRootParticle = frame => frame % ROOT_FREQUENCY === 0;
 
 const frameShouldAnimateScaleParticle = frame => frame % SCALE_FREQUENCY === 0;
 
-const frameShouldAnimateChordParticle = (frame, mode) => frame % CHORD_FREQUENCY === 0;
+const frameShouldAnimateChordParticle = frame => frame % CHORD_FREQUENCY === 0;
 
 const frameShouldFinishModeAnimation = (scaleIndex, mode) =>
     scaleIndex === getModeScaleLength(mode);

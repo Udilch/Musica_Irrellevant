a61667b2a8591b2738fc366f7a640ba17f6686fb
Odil Bright <MacbookPro@iMac-de-Prueba.local>
2019-09-13 00:49:42 +0200

    UpdatedCredits

--- Info.js
+++ Info.js
@@ -14,7 +14,7 @@ export const infoExpand = () => {
     info.parent(button);
     info.style('text-align','left');
     info.style('clear', 'both');
-    info.html('<p>&#34;<a href="https://github.com/Udilch/QuantumParticles" target="_blank">Música Irrellevant</a>&#34; is an interactive application made by <a href="https://odilbright.com/" target="_blank">Odil Bright</a>.</p><p>Special thanks to <a href="http://andypoole.info/" target="_blank">Andy Poole</a> for his technical assistance with the software architecture and design.</p>');
+    info.html('<p>&#34;<a href="https://github.com/Udilch/QuantumParticles" target="_blank">Música Irrellevant</a>&#34; is an interactive application by <a href="https://odilbright.com/" target="_blank">Odil Bright</a>, developed using <a href="https://tonejs.github.io">Tone.js</a> and <a href="https://p5js.org">p5.js</a>.</p><p>Special thanks to <a href="http://andypoole.info/" target="_blank">Andy Poole</a> for his technical assistance with the software architecture and design.</p>');
     button.mouseClicked(infoCollapse);
 }
 
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { setup, draw } from './Sketch.js';
 import { windowResized } from './Canvas.js';
-import img from './Images/ActivarSo.svg';
 import p5 from 'p5/lib/p5.min.js';
 import p5dom from 'p5/lib/Addons/p5.dom.js';
 import style from './style.css';
